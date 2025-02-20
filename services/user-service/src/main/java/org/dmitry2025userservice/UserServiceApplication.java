77419040fffded26304851eb8a50ea26a6dd2bbb
DmitryV13 <voiku173dmitry@gmail.com>
2025-02-20 02:58:12 +0200

    secrets were added

--- services/user-service/src/main/java/org/dmitry2025userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/org/dmitry2025userservice/UserServiceApplication.java
@@ -3,7 +3,7 @@ package org.dmitry2025userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+//repo secrets were added
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
